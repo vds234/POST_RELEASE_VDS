b6a42adc5f1d08f0620f70f61d79e67813f690b0
vds234 <viveksingh.vds@gmail.com>
2026-02-03 19:35:34 +0530

    Atomic Synch lock free sync

---
+++ Multi-Threading/WithoutAtomic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+std::atomic<int> shared_data = 0;
+void increament()
+{
+    ++shared_data;
+    std::cout << "Shared Data:" << shared_data << std::endl;
+}
+
+int main()
+{
+    std::thread t1(increament);
+    std::thread t2(increament);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
