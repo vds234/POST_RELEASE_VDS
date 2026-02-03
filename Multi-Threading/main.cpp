b4d68299d167d0c0252a2946e064e1bd953041c6
vds234 <viveksingh.vds@gmail.com>
2026-02-03 18:59:15 +0530

    Simple Program

--- Multi-Threading/main.cpp
+++ Multi-Threading/main.cpp
@@ -1,27 +1,21 @@
-//Multi-threaded application without thread safe.
-//Simple thread creation
+//Simple Multi-threading program without thread safety.
 #include <iostream>
 #include <thread>
-#include <mutex>
-std::mutex mtx;
 
-void display1()
+void printNumbers()
 {
-    mtx.lock();
-    for(int i=0;i<10000;i++)
+    for(int i=0;i<100;i++)
     {
-        
-        std::cout << i << std::endl;
-        
+        std::cout << "Number:" << i << std::endl;
     }
-    mtx.unlock();
 }
 
 int main()
 {
-    std::thread t1(display1);
-    std::thread t2(display1);
+    std::thread t1(printNumbers);
+    std::thread t2(printNumbers);
     t1.join();
     t2.join();
+
     return 0;
 }
