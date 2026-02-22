229f708a04ba280088a04e336d692c10c961c4a9
vds234 <viveksingh.vds@gmail.com>
2026-02-22 14:27:15 +0530

    Updated Encapsulation

--- Encapsulation/main.cpp
+++ Encapsulation/main.cpp
@@ -1,9 +1,19 @@
 //To demonstrate encapsulation principle
 //1. What is Encapsulation in C++
 //  Bundling of data and function together into a single unit i.e class
-//  Controlled access to private data members outside class
-//  Data hiding + class invariant(valid state of object)
-//  
+//  Controlled access to private data members outside class i.e getters() and setters() and constructor
+//  Data hiding + class invariant(valid state of object) i.e private or protected
+//2. How Encapsulation was achieved?
+//  Using Access specifier and classes.
+//3. why encapsulation is needed?
+//  -->Modularize the code.
+//  -->Maintainability.
+//  -->Data Security.
+//  -->Flexibilty.
+//  -->Data Consistency.
+//4. What will happen if encapsulation not used?
+//  --Data Corruption.
+//  --Invalid Object state i.e System crashes or undefined behaviour.
 #include <iostream>
 class Encapsulation
 {
@@ -33,7 +43,6 @@ class Encapsulation
             else
                 balance = balance - amt;
         }
-
 };
 
 int main()
